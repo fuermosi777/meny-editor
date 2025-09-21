2997e864d82d682dbb4f8a8d2c5f0f29693b5c08
Hao <fuermosi777@users.noreply.github.com>
2025-09-20 22:23:27 -0700

    initialize

---
+++ src/Tiptap.tsx
@@ -0,0 +1,54 @@
+// src/Tiptap.tsx
+import { useEditor, EditorContent, EditorContext } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { TaskItem, TaskList } from '@tiptap/extension-list'
+
+import { useMemo } from 'react'
+import './Tiptap.scss'
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [StarterKit, TaskList, TaskItem.configure({ nested: true })], 
+    content: `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+</p>
+<ul>
+  <li>
+    That’s a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+</p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+<p>
+  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+</p>
+<blockquote>
+  Wow, that’s amazing. Good work, boy! 👏
+  <br />
+  — Mom
+</blockquote>
+`, // initial content
+  })
+
+  // Memoize the provider value to avoid unnecessary re-renders
+  const providerValue = useMemo(() => ({ editor }), [editor])
+
+  return (
+    <EditorContext.Provider value={providerValue}>
+      <EditorContent editor={editor} />
+    </EditorContext.Provider>
+  )
+}
+
+export default Tiptap
